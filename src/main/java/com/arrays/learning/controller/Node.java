fabfc3e2de31174be0de127b8206e6c8f65af78d
Sreedhar Sivan <sreedhar85in@gmail.com>
2020-09-22 12:36:10 +1200

    Update the code with binary tree creation.

--- src/main/java/com/arrays/learning/controller/Node.java
+++ src/main/java/com/arrays/learning/controller/Node.java
@@ -2,16 +2,56 @@ package com.arrays.learning.controller;
 
 public class Node {
 	
-	int key;
+	int value;
 	
-	Node left, right;
+	Node left;
+	Node right;
 	
 	public Node(int item)
 	{
 		
-		key = item;
+		value = item;
 		left=right=null;
 	}
 	
 
+}
+
+
+class BinaryTree
+{
+	
+	Node root;
+	BinaryTree(int key)
+	{
+		
+		root=new Node(key);
+		
+		
+	}
+	
+	
+	BinaryTree()
+	{
+		
+		root = null;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(1);
+		
+		tree.root.left = new Node(2);
+		
+		tree.root.right = new Node(3);
+		
+		tree.root.left.left = new Node(4);
+		
+	
+		
+	}
+	
+	
 }
