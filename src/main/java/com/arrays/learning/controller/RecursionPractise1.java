0244287b6b62c0deb23be950dc7a09a3e541e32b
Sreedhar Sivan <sreedhar85in@gmail.com>
2020-09-29 15:19:44 +1300

    Recursion practise example 1

---
+++ src/main/java/com/arrays/learning/controller/RecursionPractise1.java
@@ -0,0 +1,23 @@
+package com.arrays.learning.controller;
+
+public class RecursionPractise1 {
+	
+	
+	static void fun(int n) {
+		
+		if(n==0) {
+			return;
+		}
+		
+		
+		System.out.println(n);
+		fun(n-1);
+		System.out.println(n);
+	}
+	
+	public static void main(String[] args) {
+		
+		fun(3);
+	}
+
+}
