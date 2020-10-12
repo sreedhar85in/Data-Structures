e2ec49d3b15c8c57934b45fe0ab2be3a66709776
Sreedhar Sivan <sreedhar85in@gmail.com>
2020-10-12 13:15:26 +1300

    Structure of a Node of a Linked List

---
+++ src/main/java/com/arrays/learning/controller/LinkedListStructureofaNode.java
@@ -0,0 +1,14 @@
+package com.arrays.learning.controller;
+
+//class node having Generic data-type <T>
+public class LinkedListStructureofaNode<T> {
+	
+	public T data; //Data to store (could be int, String, Object etc) 
+	public LinkedListStructureofaNode nextNode; //pointer to next node in list
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
