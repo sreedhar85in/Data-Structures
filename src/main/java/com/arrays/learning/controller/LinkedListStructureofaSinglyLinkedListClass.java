5ae09034a3f2f04031f1d0d342eaed666d5b2378
Sreedhar Sivan <sreedhar85in@gmail.com>
2020-10-12 13:34:02 +1300

    Structure of a Singly Linked List class.

---
+++ src/main/java/com/arrays/learning/controller/LinkedListStructureofaSinglyLinkedListClass.java
@@ -0,0 +1,27 @@
+package com.arrays.learning.controller;
+
+public class LinkedListStructureofaSinglyLinkedListClass<T> {
+
+	public class Node{
+		
+		public T data;
+		public Node nextNode;
+		
+	}
+	
+	public Node headNode;
+	public int size;
+	//constructor
+	
+	public LinkedListStructureofaSinglyLinkedListClass() {
+		headNode=null;
+		size=0;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
