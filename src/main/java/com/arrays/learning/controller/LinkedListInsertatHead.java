183e78d68729201fc5ee9d18db34e64614651a3b
Sreedhar Sivan <sreedhar85in@gmail.com>
2020-10-13 15:32:13 +1300

    Inserting the elements at Head of a linked list program

---
+++ src/main/java/com/arrays/learning/controller/LinkedListInsertatHead.java
@@ -0,0 +1,79 @@
+package com.arrays.learning.controller;
+
+import java.util.LinkedList;
+
+public class LinkedListInsertatHead<T> {
+
+	public class Node{
+		
+		public T data;
+		public Node nextNode;
+		
+	}
+	
+	public Node headNode;
+	public int size;
+	//LinkedList test = new LinkedList();
+	//constructor
+	
+	public LinkedListInsertatHead() {
+		headNode=null;
+		size=0;
+		
+	}
+	
+	public  boolean isEmpty() {
+		
+		if (headNode == null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void insertAtHead(T data) {
+		//creating a new node and assigning it the new data value
+		
+		Node newNode= new Node();
+		newNode.data= data;
+		newNode.nextNode = headNode;
+		headNode = newNode;
+		size++;
+	}
+	
+	//Helper fundtion to printList
+	
+	public void printList() {
+		
+		if(isEmpty()) {
+			System.out.println("List is Empty");
+			return;
+		}
+		
+		Node temp = headNode;
+		System.out.print("List : ");
+		while(temp.nextNode!=null) {
+			System.out.print(temp.data.toString() + " -> ");
+			temp=temp.nextNode;
+		}
+		System.out.println(temp.data.toString() + " -> null");
+	}
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedListInsertatHead obj = new LinkedListInsertatHead();
+		boolean test =obj.isEmpty();
+		System.out.println("the value of the boolean is :" + test);
+		obj.printList();
+		for(int i=1;i<=10; i++) {
+			
+			obj.insertAtHead(i);
+			obj.printList();
+		}
+		
+	}
+
+}
