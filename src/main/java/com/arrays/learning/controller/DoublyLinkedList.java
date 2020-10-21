fb800abf8d113ef1159d8d3e6b4875f69dd7adb9
Sreedhar Sivan <sreedhar85in@gmail.com>
2020-10-21 13:57:51 +1300

    Doubly Linked List structure of a class and Deletion by value in a Doubly Linked List.

---
+++ src/main/java/com/arrays/learning/controller/DoublyLinkedList.java
@@ -0,0 +1,74 @@
+package com.arrays.learning.controller;
+
+public class DoublyLinkedList<T> {
+	
+	
+	public class Node{
+		
+		public T data;
+		public Node nextNode;
+		public Node prevNode;
+		
+		
+	}
+	
+	public Node headNode;
+	public int size;
+
+	public DoublyLinkedList() {
+		this.headNode = null;
+	}
+	
+	
+	public boolean isEmpty() {
+		if (headNode == null) {
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public void deletebyValue(T data) {
+		
+		if(isEmpty()) {
+			return;
+		}
+		
+		//start from head node
+		
+		Node currentNode = this.headNode;
+		
+		if(currentNode.data.equals(data)) {
+			//found the elemenet at head. so used delete at head method.
+			return;
+		}
+		
+		//traverse the list searching for the data to delete
+		while(currentNode!=null) {
+			
+			if(data.equals(currentNode.data)) {
+				
+				currentNode.prevNode.nextNode = currentNode.nextNode;
+			
+			
+			if(currentNode.nextNode!=null){
+				currentNode.nextNode.prevNode = currentNode.prevNode;				
+			}
+			
+			size--;
+			return;
+			}
+			currentNode = currentNode.nextNode;
+		}
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
